44f20724a976b9d23f34cc680dd147b5f640dea6
CodeWithLychee <abhi.sharma10220@gmail.com>
2024-08-27 12:16:10 +0530

    fix

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -11,6 +11,7 @@ import { useNavigate } from "react-router-dom";
 
 function SignIn() {
   const navigate = useNavigate();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
